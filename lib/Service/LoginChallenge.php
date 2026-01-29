fb66bb57b2d5b21703991cd1fa2d7a1d3e1f8da6
Olav Seyfarth <olav@seyfarth.de>
2026-01-30 00:08:52 +0100

    Use bcrypt to store code in preferences (inspired by PR#14)
    Signed-off-by: Olav Seyfarth <olav@seyfarth.de>

--- lib/Service/LoginChallenge.php
+++ lib/Service/LoginChallenge.php
@@ -20,15 +20,43 @@ final class LoginChallenge implements ILoginChallenge {
 	}
 
 	public function sendChallenge(IUser $user): void {
-		$code = $this->codeGenerator->generateChallengeCode();
-		$this->codeStorage->writeCode($user->getUID(), $code);
-		$this->emailSender->sendChallengeEMail($user, $code);
+		$generatedCode = $this->codeGenerator->generateChallengeCode();
+
+		// Is it OK to define the algo and data structure here in these two functions?
+		// We want to keep it simple, and to pull from AppSettings seems a lot of overhead!
+
+		// the code was stored as plain text (6 digits) before, without a delimiter
+		// we deem all such codes invalid and ignore them
+
+		// We now use a structured string to denote in which form the code was stored, with ':' as delimiter.
+		// code := [algorithm identifier].':'.[representation of the code, e.g. its hash]
+
+		// list of yet defined algorithm identifiers:
+		//   PBC = password_hash($code, PASSWORD_BCRYPT) – currently in use
+
+		$hashedCode = 'PBC' . password_hash($generatedCode, PASSWORD_BCRYPT);
+
+		$this->codeStorage->writeCode($user->getUID(), $hashedCode);
+		$this->emailSender->sendChallengeEMail($user, $generatedCode);
 	}
 
 	public function verifyChallenge(IUser $user, string $submittedCode): bool {
 		$submittedCode = trim($submittedCode);
-		$code = $this->codeStorage->readCode($user->getUID());
-		$isValid = $submittedCode === $code;
+		$storedCode = $this->codeStorage->readCode($user->getUID());
+
+		$array = preg_split(":", $storedCode, NULL, PREG_SPLIT_NO_EMPTY);
+		if ($array) {
+			$isValid = match ($array[0]) {
+				"PBC" => password_hash($submittedCode, PASSWORD_BCRYPT) === $storedCode,
+				default => false, // unknown algorithm identifier → discard
+			};
+		} else {
+			// preg_split either did not find a delimiter (":") in the stored string or one field was empty
+			$isValid = false;
+		}
+
+		// ToDo: Decide: Should we delete the code as soon as it was used once?The user could re-login…
+		//               The user would then need to start over the whole login process …
 		if ($isValid) {
 			$this->codeStorage->deleteCode($user->getUID());
 		}
