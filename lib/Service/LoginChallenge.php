4ee96d49004af8476bcdad484edba117862b01a8
Olav Seyfarth <olav@seyfarth.de>
2026-01-30 23:21:46 +0100

    simplify code, cleanup comments
    Signed-off-by: Olav Seyfarth <olav@seyfarth.de>

--- lib/Service/LoginChallenge.php
+++ lib/Service/LoginChallenge.php
@@ -23,48 +23,17 @@ final class LoginChallenge implements ILoginChallenge {
 
 	public function sendChallenge(IUser $user): void {
 		$generatedCode = $this->codeGenerator->generateChallengeCode();
-
-		// Is it OK to define the algo and data structure here in these two functions?
-		// We want to keep it simple, and to pull from AppSettings seems a lot of overhead!
-
-		// the code was stored as plain text (6 digits) before, without a delimiter
-		// we deem all such codes invalid and ignore them
-
-		// We now use a structured string to denote in which form the code was stored, with ':' as delimiter.
-		// code := [algorithm identifier].':'.[representation of the code, e.g. its hash]
-
-		// list of yet defined algorithm identifiers:
-		//   PBC = password_hash($code, PASSWORD_BCRYPT) – currently in use
-
-		$hashedCode = 'PBC:' . $this->hasher->hash($generatedCode);
-		$this->codeStorage->writeCode($user->getUID(), $hashedCode);
+		// to harden in case of privilege escalation, store code as hash using a method resistent to time-based attacks
+		$this->codeStorage->writeCode($user->getUID(), $this->hasher->hash($generatedCode));
 		$this->emailSender->sendChallengeEMail($user, $generatedCode);
 	}
 
 	public function verifyChallenge(IUser $user, string $submittedCode): bool {
 		$submittedCode = trim($submittedCode);
-		$storedCode = $this->codeStorage->readCode($user->getUID());
+		$storedCodeHash = $this->codeStorage->readCode($user->getUID());
+		$isValid = $this->hasher->verify($submittedCode, $storedCodeHash);
 
-		if (! is_null($storedCode)) {
-			$array = explode(':', $storedCode, 2);
-			if (count($array) === 2) {
-				$isValid = match ($array[0]) {
-					'PBC' => $this->hasher->verify($submittedCode, $array[1]),
-					default => false, // unknown algorithm identifier → discard
-				};
-			} else {
-				// preg_split either did not find a delimiter (":") in the stored string or one field was empty
-				$isValid = false;
-				// ToDo: Should The user be notified? Should these cases then be handled differently?
-			}
-		} else {
-			$isValid = false; // readCode did not return a code
-			// ToDo: This should never happen since a code should have been saved prior to verifying it
-			//       So should this be logged as warning* Should the user be notified (about what)?
-		}
-
-		// ToDo: Should we delete the code as soon as it was used once?The user could re-login…
-		//       The user would then need to start over the whole login process …
+		// We could always delete the code here but this way it is more convenient for users (in case of a mistype).
 		if ($isValid) {
 			$this->codeStorage->deleteCode($user->getUID());
 		}
