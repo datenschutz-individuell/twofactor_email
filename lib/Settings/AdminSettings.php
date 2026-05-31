7896668b9f13fb794b728eb74910d2fd1020d83b
Olav Seyfarth <olav@seyfarth.de>
2026-05-31 12:39:52 +0200

    Revert "Revert to ISettings"
    This reverts commit 82f0232ecde96589746f6084dd2c5e4b4abc6176.

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -14,35 +14,43 @@ use OCA\TwoFactorEMail\Service\IAppSettings;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Services\IInitialState;
 use OCP\IL10N;
-use OCP\Settings\ISettings;
+use OCP\Settings\IDelegatedSettings;
 
-class AdminSettings implements ISettings {
-    public function __construct(
-        private IAppSettings $appSettings,
-        private IInitialState $initialState,
-        private IL10N $l10n,
-    ) {
-    }
+class AdminSettings implements IDelegatedSettings {
+	public function __construct(
+		private IAppSettings $appSettings,
+		private IInitialState $initialState,
+		private IL10N $l10n,
+	) {
+	}
 
-    public function getForm(): TemplateResponse {
-        $this->initialState->provideInitialState('codeLength', $this->appSettings->getCodeLength());
-        $this->initialState->provideInitialState('codeValidMinutes', $this->appSettings->getCodeValidMinutes());
-        $this->initialState->provideInitialState('sendRateLimitAttempts', $this->appSettings->getSendRateLimitAttempts());
-        $this->initialState->provideInitialState('sendRateLimitPeriodSeconds', $this->appSettings->getSendRateLimitPeriodSeconds());
-        $this->initialState->provideInitialState('eMailTemplate', $this->appSettings->getEMailTemplate());
+	public function getForm(): TemplateResponse {
+		$this->initialState->provideInitialState('codeLength', $this->appSettings->getCodeLength());
+		$this->initialState->provideInitialState('codeValidMinutes', $this->appSettings->getCodeValidMinutes());
+		$this->initialState->provideInitialState('sendRateLimitAttempts', $this->appSettings->getSendRateLimitAttempts());
+		$this->initialState->provideInitialState('sendRateLimitPeriodSeconds', $this->appSettings->getSendRateLimitPeriodSeconds());
+		$this->initialState->provideInitialState('eMailTemplate', $this->appSettings->getEMailTemplate());
 
-        return new TemplateResponse(Application::APP_ID, 'AdminSettings', renderAs: TemplateResponse::RENDER_AS_BLANK);
-    }
+		return new TemplateResponse(Application::APP_ID, 'AdminSettings', renderAs: TemplateResponse::RENDER_AS_BLANK);
+	}
 
-    public function getSection(): string {
-        return 'security';
-    }
+	public function getSection(): string {
+		return 'security';
+	}
 
-    public function getPriority(): int {
-        return 30;
-    }
+	public function getPriority(): int {
+		return 30;
+	}
 
-    public function getName(): ?string {
-        return $this->l10n->t('Email');
-    }
+	public function getName(): ?string {
+		return $this->l10n->t('Email');
+	}
+
+	public function getAuthorizedGroupIds(): array {
+		return []; // an empty array means "real admins only, no delegated/group admins"
+	}
+
+	public function getAuthorizedAppConfig(): array {
+		return [];  // no app config keys delegated to non-admins
+	}
 }
