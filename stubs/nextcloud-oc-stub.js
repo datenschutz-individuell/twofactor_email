382eb9085b40a7c14e185699e0f282bb65ce0197
Olav Seyfarth <olav@seyfarth.de>
2026-06-11 03:08:42 +0200

    Fix REUSE
    Signed-off-by: Olav Seyfarth <olav@seyfarth.de>

--- stubs/nextcloud-oc-stub.js
+++ stubs/nextcloud-oc-stub.js
@@ -1,2 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Olav and Niklas Seyfarth, Contributors <https://github.com/datenschutz-individuell/twofactor_email/blob/main/CONTRIBUTORS.md>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 // eslint-disable-next-line no-unused-vars
 const OC = {}
